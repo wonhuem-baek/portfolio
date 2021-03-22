8f783a00f8bdc0729245338904b06adebebc0576
wonhuem-baek <whpaik67@gmai.com>
2021-03-21 18:55:35 -0700

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -19,76 +19,7 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove('open');
-  scrollIntoView(link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior:"smooth"});
 });
 
-// Navbar toggle button for small screen
-const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-navbarToggleBtn.addEventListener('click', () => {
-  navbarMenu.classList.toggle('open');
-});
-
-// Handle click on "contact me" button on home
-const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', () => {
-  scrollIntoView('#contact');
-});
-
-// Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
-});
-
-// Show "arrow up" button when scrolling down
-const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', () => {
-  if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add('visible');
-  } else {
-    arrowUp.classList.remove('visible');
-  }
-});
-
-// Handle click on the "arrow up" button
-arrowUp.addEventListener('click', () => {
-  scrollIntoView('#home');
-});
-
-// Projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
-  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
-  }
-
-  // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
-  if (active != null) {
-    active.classList.remove('selected');
-  }
-  e.target.classList.add('selected');
-
-  projectContainer.classList.add('anim-out');
-  setTimeout(() => {
-    projects.forEach((project) => {
-      console.log(project.dataset.type);
-      if (filter === '*' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
-      } else {
-        project.classList.add('invisible');
-      }
-    });
-    projectContainer.classList.remove('anim-out');
-  }, 300);
-});
-
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
